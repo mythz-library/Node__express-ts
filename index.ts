87328a9b016d5c1b6d8b37bfee61323b389ec73a
Ashen Jayarathne <ashenjayarathnedev@gmail.com>
2024-11-24 17:15:17 +0530

    Implement post req and parse req bodies

--- index.ts
+++ index.ts
@@ -1,15 +1,18 @@
-import express from 'express';
-import reminderRouter from './routers/reminders'
+import express from "express";
+import reminderRouter from "./routers/reminders";
 
 const app = express();
 
+// Middleware
+app.use(express.json());
 
-app.use('/reminders', reminderRouter)
+// Routers
+app.use("/reminders", reminderRouter);
 
-app.get('/', (req, res) => {
-    res.status(200).send("API working")
-})
+app.get("/", (req, res) => {
+  res.status(200).send("API working");
+});
 
 app.listen(8000, () => {
-    console.log('Server Started')
-})
+  console.log("Server Started");
+});
