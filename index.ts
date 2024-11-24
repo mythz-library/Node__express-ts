6139f87e73c929857bedc30f99546057cf7a605b
Ashen Jayarathne <ashenjayarathnedev@gmail.com>
2024-11-24 16:37:07 +0530

    Implement basic route

--- index.ts
+++ index.ts
@@ -2,6 +2,11 @@ import express from 'express';
 
 const app = express();
 
+
+app.get('/', (req, res) => {
+    res.status(200).send("API working")
+})
+
 app.listen(8000, () => {
     console.log('Server Started')
 })
