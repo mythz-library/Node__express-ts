b6f3f6a80ce57271648a040b6e73240f5658f0ee
Ashen Jayarathne <ashenjayarathnedev@gmail.com>
2024-11-24 16:55:57 +0530

    Creating a router

--- index.ts
+++ index.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import reminderRouter from './routers/reminders'
 
 const app = express();
 
+
+app.use('/reminders', reminderRouter)
 
 app.get('/', (req, res) => {
     res.status(200).send("API working")
